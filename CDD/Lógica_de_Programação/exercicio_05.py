cd98893b6aba774a09da54bd6674d80c8f7c8948
Dhakson <dhaksonbarbosa94@gmail.com>
2024-06-09 22:59:22 -0300

    calculo com as 4 operações matemática

---
+++ CDD/Lógica_de_Programação/exercicio_05.py
@@ -0,0 +1,12 @@
+n1 = 4
+n2 = 8
+
+adicao = n1 + n2
+subtracao = n1 - n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+
+print(f'{n1} + {n2} = ',adicao)
+print(f'{n1} - {n2} = ',subtracao)
+print(f'{n1} / {n2} = ',divisao)
+print(f'{n1} * {n2} = ',multiplicacao)
